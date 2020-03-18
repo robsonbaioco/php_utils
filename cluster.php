47c18e9d4df72686f5f3601cba5867dee2ba6d8a
Robson Samir Baioco <rsbaioco@gmail.com>
2020-03-18 14:07:22 -0300

    create cluster.php

---
+++ cluster.php
@@ -0,0 +1,9 @@
+<?php
+  
+  function _group_by($array, $key) {
+        $return = array();
+        foreach($array as $val) {
+            $return[$val[$key]][] = $val;
+        }
+        return $return;
+    }
